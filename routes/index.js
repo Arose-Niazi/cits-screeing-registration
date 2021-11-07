a02135fb79c280016e0da1047eb314cede3f502e
Arose Niazi <arose.niazi@yahoo.com>
2021-11-07 17:04:23 +0500

    chnages

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,50 @@
+const mysql = require('mysql2/promise');
 var express = require("express");
 var router = express.Router();
 var QRCode = require('qr-image');
 
+const connectionObject = {
+  host: "51.210.180.137",
+  user: "cits",
+  password: "PLUY0P3Du0bNz2iN",
+  database: "cits",
+};
+
+router.get("/users", async function (req, res, next) {
+  const connection = await mysql.createConnection(connectionObject);
+  const [rows, fields] = await connection.query('SELECT * FROM semi');
+  const visited = rows.filter((value) => value.Arrived == 1);
+
+  res.render("users", { users: rows, visited });
+});
+
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  if (req.query.ID) {
-    let qr_png = QRCode.image(JSON.stringify({ ID: req.query.ID, fName: req.query.fName, lName: req.query.lName }), { type: 'png' });
-    await qr_png.pipe(require('fs').createWriteStream('public/Images/QR/' + req.query.ID + '.png'));
-    res.render("index", { ID: req.query.ID });
+  var ID = req.query.ID ? req.query.ID : null;
+  var fName = req.query.fName ? req.query.fName : '';
+  var lName = req.query.lName ? req.query.lName : '';
+  if (ID) {
+    const connection = await mysql.createConnection(connectionObject);
+    if (fName.length > 1) {
+
+      connection.query('INSERT IGNORE INTO semi VALUES (?,?,?,0);', [ID, fName, lName]);
+      let qr_png = QRCode.image(JSON.stringify(`https://cits.arose-niazi.me/?ID=FA18-BSE-010`), { type: 'png' });
+      await qr_png.pipe(require('fs').createWriteStream('public/Images/QR/' + req.query.ID + '.png'));
+      res.render("index", { ID, fName });
+    }
+    else {
+      const [rows, fields] = await connection.query('SELECT * FROM semi WHERE ID=? LIMIT 1', [ID]);
+      console.log(rows);
+      res.render("view", rows[0]);
+      connection.query('UPDATE semi SET Arrived=1 WHERE ID=? LIMIT 1', [ID]);
+    }
+
   }
   else
-    res.send("Nothing to see");
+    res.render('poster');
 });
+
+
+
+
 module.exports = router;
